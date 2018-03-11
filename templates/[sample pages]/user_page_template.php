300cd2a3582f09c64e8937d4b60a263999c83683
Daniel Lachica <daniel.lachica82@gmail.com>
2018-03-11 15:32:10 +0800

    Added new favicon image

---
+++ templates/[sample pages]/user_page_template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Shoeversity</title>
+
+    <?php
+        // Include Bootstrap and main styles 
+        include "../templates/users/user_bs_styles.php";
+        include "../templates/users/user_shoeversity_styles.php";
+    ?>
+</head>
+<body>
+    
+
+<!-- Include Javascript files -->
+</body>
+</html>
