1bd8a99bbaf381e40d3cb8fc70a5db8c5e5d47e0
Daniel Lachica <daniel.lachica82@gmail.com>
2018-03-11 13:47:18 +0800

    Added project base files
    I added the following files + folders:
    
    - CSS for footer, header, main, and public (empty sheets)
    - templates linking to above css files (to be used on appropriate pages)
    - blank file templates for Admin, User, Brand, and Public pages (can be found in Shoeversity/templates/[sample pages])
    - Added favicon link to all pages

--- database/config.php
+++ database/config.php
@@ -5,8 +5,6 @@ $DB_USERNAME = "root";
 $DB_PASSWORD = "";
 $DB_NAME     = "shoeversity";
 
-
-
 /* Connect to MySQL Database */
 $conn = mysqli_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME); 
 
---
+++ templates/public_bs_styles.php
@@ -0,0 +1,14 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Include Bootstrap (local) and CSS files -->
+<link rel="stylesheet" href="../bootstrap-3.3.7/css/bootstrap.min.css">
+<!-- Include jQuery -->
+<script src="../js/jquery-3.3.1.min.js"></script>
+<!-- Include Bootstrap JavaScript -->
+<script src="../bootstrap-3.3.7/js/bootstrap.min.js"></script>
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
