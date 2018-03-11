8e30dca3a12669c80e83d26e4a7ca09d7388a62b
Daniel Lachica <daniel.lachica82@gmail.com>
2018-03-11 12:10:11 +0800

    Added config.php to Shoeversity/database
    config.php will be our default MySQL connector file

---
+++ database/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$DB_HOST 	 = "localhost";
+$DB_USERNAME = "root";
+$DB_PASSWORD = "";
+$DB_NAME 	 = "shoeversity";
+
+/* Connect to MySQL Database */
+$conn = mysqli_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME); 
+
+?>
