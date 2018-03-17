3db3fdf7da6925a6469103bfcd2ab39d51b1819a
Daniel Lachica <daniel.lachica82@gmail.com>
2018-03-17 23:10:15 +0800

    Fixed carousel issue in index

--- views/index.php
+++ views/index.php
@@ -61,11 +61,11 @@
             </div>
 
             <!-- Left and right controls -->
-            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <a class="left carousel-control" href="#shoeversityCarousel" data-slide="prev">
                 <span class="glyphicon glyphicon-chevron-left"></span>
                 <span class="sr-only">Previous</span>
             </a>
-            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <a class="right carousel-control" href="#shoeversityCarousel" data-slide="next">
                 <span class="glyphicon glyphicon-chevron-right"></span>
                 <span class="sr-only">Next</span>
             </a>
