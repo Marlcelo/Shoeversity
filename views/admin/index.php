a5107229801439f9a43212e1c28177812527c7f0
Chelsey <chelsey_medina@dlsu.edu.ph>
2018-03-17 23:56:08 +0800

    Added layout and correct template file locations

--- views/admin/index.php
+++ views/admin/index.php
@@ -6,17 +6,73 @@
 
     <?php
         // Include Bootstrap and main styles 
-        include "../templates/admin/admin_bs_styles.php";
-        include "../templates/admin/admin_shoeversity_styles.php";
+        include "../../templates/admin/admin_bs_styles.php";
+        include "../../templates/admin/admin_shoeversity_styles.php";
     ?>
 </head>
 <body>
 	<!-- Include header -->
-	<?php include "../templates/admin/admin_header.php"; ?>
+	<?php include "../../templates/admin/admin_header.php"; ?>
 	
+    <!-- BEGIN MAIN CONTENT -->
+
+    <div id="shoeversityCarousel" class="carousel slide" data-ride="carousel" style="position: absolute; top: 0; margin-bottom: 100px; width: 100%; height: 100vh">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#shoeversityCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#shoeversityCarousel" data-slide-to="1"></li>
+                <li data-target="#shoeversityCarousel" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                    <div class="fill">
+                        <img src="https://www.metaslider.com/wp-content/uploads/2014/11/mountains1.jpg" style="height: 100vh;">
+                    </div>
+                    <div class="carousel-caption">
+                        <h1>WELCOME TO SHOEVERSITY</h1>
+                        <h3>Your one stop for all shoe needs.</h3>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="fill">
+                        <img src="http://www.food4fuel.com/wp-content/uploads/2014/02/rebound-slider-2-bg.jpg" style="height: 100vh;">
+                    </div>
+                    <div class="carousel-caption">
+                        <p><h3><em>Outfits aren't complete without them sneakas!</em></h3></p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <div class="fill">
+                        <img src="https://www.procloud.com.au/wp-content/uploads/2016/08/slider-dark-bg.jpg" style="height: 100vh;">
+                    </div>
+                    <div class="carousel-caption">
+                        <h3>Explore what we have to offer.</h3>
+                        <p>We want to let you discover various brands' products and create your OOTD.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>        
+    </div>
+
     <div class="content-wrapper ">
             ADMIN PAGE
 	</div>
+
+    <!-- END OF MAIN CONTENT -->
 
 	<!-- Include Javascript files -->
 </body>
