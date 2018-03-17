2296b824d4a5fc418b555bf5e777a782fbb73ef5
Marl Christian Ricanor <32050672+Marlcelo@users.noreply.github.com>
2018-03-17 22:47:01 +0800

    Store user to site_users table

--- database/user_register.php
+++ database/user_register.php
@@ -19,15 +19,37 @@
 
 			echo "$query";
 
-			$result = mysqli_query($conn,$query) or die(mysql_error($conn));
+			$result = mysqli_query($conn,$query) or die(mysqli_error($conn));
 
 			$row = mysqli_fetch_assoc($result);
 			var_dump($row);
 
 			if($row['col'] == 'SUCCESS')
-				echo "STORED";
+				echo "STORED USER INFORMATION";
 			else
 				echo "ERROR";	// For Checking purposes
+
+//*******************************************************
+// STORE TO SITE_USERS TABLE
+//********************************************************
+			mysqli_close($conn);
+			require 'config.php';
+
+			$query = "CALL SP_ADD_SITE_USER('User','".$uname."',FN_GET_HASHEDPASSWORD('".$pass."'));";
+
+			echo "$query";
+
+			$result = mysqli_query($conn,$query) or die(mysqli_error($conn));
+
+			$row = mysqli_fetch_assoc($result);
+			var_dump($row);
+
+			if($row['col'] == 'SUCCESS')
+				echo "STORED USER SITE USER";
+			else
+				echo "ERROR";
+
+
 
 			header("Location: ../index.php"); //PLEASE CHANGE THIS TO USER LANDING PAGE
 		}
