ff88b825e90246df626c1ce41586ecd80370cea6
Chelsey <chelsey_medina@dlsu.edu.ph>
2018-03-19 23:46:47 +0800

    Added create an admin button

--- views/admin/account.php
+++ views/admin/account.php
@@ -23,8 +23,41 @@
 	<?php include "../../templates/admin/admin_header.php"; ?>
 	
     <div class="content-wrapper ">
-        ADMIN ACCOUNT
-	</div>
+        <div class="container text-center">
+                <div class="col-md-4">
+                    <img class="img-circle" src="../IMAGES/USERS/dp.jpg" width="160px" alt="profilepic">
+                    <br><br><br>
+                    <label for="uname">Username: </label>
+                    <br>
+                    
+                    <hr>
+                    <label for="name">Name:
+                    </label>
+                    <br>
+                    
+                    <hr>
+                    <label for="email">Email:</label>
+                    <br>
+                    
+                    <hr>
+                    <label for="gnder">Gender:</label>
+                    <br>
+                    
+                    <hr>
+                    <a href=""><button class="btn-md btn-info">Create an Admin Account</button></a>
+                </div>
+                <div class="col-md-8">
+                    <h2>List of Admins</h2><hr><br>
+
+                    <br><br>
+                    <h2>Recent Transactions</h2><hr><br>
+
+                    <br><br>
+                    <h2>Recent B</h2><hr><br>
+        
+                </div>              
+        </div>
+    </div>
 
 	<!-- Include Javascript files -->
 </body>
