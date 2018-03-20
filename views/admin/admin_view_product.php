f8efb70869590b356054f0186639f85441450633
Chelsey <chelsey_medina@dlsu.edu.ph>
2018-03-20 23:20:36 +0800

    Added linking to view product

--- views/admin/admin_view_product.php
+++ views/admin/admin_view_product.php
@@ -42,10 +42,7 @@
     					</div>
     					<div class="details col-md-6">
                             <div class="row" >
-                                <div class="col-md-10 col-sm-10">
-                                <a href="edit_product.php"><button class="btn btn-md btn-info pull-right" style="height:45px; width: 70px;"><i class="glyphicon glyphicon-edit"></i></button></a>
-                                </div>
-                                <div class="col-md-2 col-sm-2">
+                                <div class="col-md">
                                 <a href="remove_product.php"><button class="btn btn-md btn-info pull-right" style="height:45px; width: 70px;"><i class="glyphicon glyphicon-remove"></i></button></a><br>
                                 </div>
                             </div>
