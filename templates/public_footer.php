81078383e324c3fe12102477806d709cf0e0c3cf
Chelsey <chelsey_medina@dlsu.edu.ph>
2018-03-20 22:20:45 +0800

    Finalized public view product layout

--- templates/public_footer.php
+++ templates/public_footer.php
@@ -1,81 +1,68 @@
-<!-- <footer class="navbar navbar-bottom shoeversity-footer">
-	<div class="container text-center">
-		<ul class="list-inline footer-content-left">
-			<li>&copy; <p id="copyright_year" style="display: inline;"></p> On Semiconductor Philippines Inc.</li>
-			&nbsp;&nbsp;
-			<li>
-				<a data-toggle="tab" href="#register">Register Trip</a>
-			</li>
-			<li>
-				<a data-toggle="tab" href="#routes">Routes</a>
-			</li>
-			<li>
-				<a data-toggle="tab" href="#map">Map</a>
-			</li>
-			<li>
-				<a data-toggle="tab" href="#busmarshals">Bus Marshals</a>
-			</li>
-		</ul>
+<!DOCTYPE html>
+<style type="text/css">
+  .myFooter {
+    border-top: 1px solid #efefef;
+    background: #f0f0f0;
+    height: 82px;
+    width: 100%;
+    margin: 0 auto;
+    margin-top: 70px;
+    padding: 25px;
+  }
+</style>
 
-		<ul class="list-inline footer-content-right">
-			<li>  
-				<i><p class="text-default bg-dark pull-right">For concerns, contact IT @ Local 727</p></i>
-			</li>
-		</ul>
-	</div>
-</footer>         -->
-
-<footer class="footer" id="footer" style="padding-bottom: 0px;">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <h3> Contact </h3>
-                <ul>
-                    <li> <a href="#"> Lorem Ipsum </a> </li>
-                    <li> <a href="#"> Lorem Ipsum </a> </li>
-                    <li> <a href="#"> Lorem Ipsum </a> </li>
-                    <li> <a href="#"> Lorem Ipsum </a> </li>
-                </ul>
-            </div>
-            <div class="col-md-4">
-                <h3> Important Links </h3>
-                <ul>
-                    <li> <a href="#"> Admission </a> </li>
-                    <li> <a href="#"> Academic </a> </li>
-                    <li> <a href="#"> Career </a> </li>
-                    <li> <a href="#"> Administration </a> </li>
-                    <li> <a href="#"> Notice </a> </li>
-                    <li> <a href="#"> Tender </a> </li>
-                    <li> <a href="login.php"> Teacher Login </a> </li>
-                </ul>
-            </div>
-            <div class="col-md-4">
-                <h3> Location </h3>
-                <ul>
-                    <li> 
-                     <form action="" class="navbar-form" role="search" style="width: 100%">
-                        <div class="input-group" style="margin-left: -15px">
-                           <input type="text" class="form-control" placeholder="Search" name="search-product">
-                           <div class="input-group-btn">
-                              <button class="btn btn-primary" type="submit" style="margin-top: 0px">
+ <!--Footer-->
+ <footer class="footer" id="footer" style="padding-bottom: 0px;">
+      
+  <!--Footer Links-->
+  <div class="container text-center text-md-left">
+      <div class="row">
+          <div class="col-md-4">
+              <h3> Contact </h3>
+              <ul>
+                  <li> <a href="#"> Lorem Ipsum </a> </li>
+                  <li> <a href="#"> Lorem Ipsum </a> </li>
+                  <li> <a href="#"> Lorem Ipsum </a> </li>
+                  <li> <a href="#"> Lorem Ipsum </a> </li>
+              </ul>
+          </div>
+          <div class="col-md-4">
+              <h3> Important Links </h3>
+              <ul>
+                  <li> <a href="#"> Admission </a> </li>
+                  <li> <a href="#"> Academic </a> </li>
+                  <li> <a href="#"> Career </a> </li>
+                  <li> <a href="#"> Administration </a> </li>
+                  <li> <a href="#"> Notice </a> </li>
+                  <li> <a href="#"> Tender </a> </li>
+                  <li> <a href="login.php"> Teacher Login </a> </li>
+              </ul>
+          </div>
+          <div class="col-md-4">
+              <h3> Location </h3>
+              <ul>
+                  <li> 
+                    <form action="" class="navbar-form" role="search" style="width: 100%">
+                      <div class="input-group" style="margin-left: -15px">
+                        <input type="text" class="form-control" placeholder="Search" name="search-product">
+                            <div class="input-group-btn">
+                                <button class="btn btn-primary" type="submit" style="margin-top: 0px">
                                 <i class="glyphicon glyphicon-search"></i>
-                              </button>
-                          </div>
-                      </div>
-                  </form>
-              </li>
-              <li> <a href="#"> Shop </a> </li>
-              <li> <a href="#"> Sign In </a> </li>
-              <li> <a href="#"> Create an Account </a> </li>
-          </ul>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </li>
+                <li> <a href="../views/index.php"> Products </a> </li>
+                <li> <a href="../views/login.php"> Login </a> </li>
+                <li> <a href="../views/register.php"> Register </a> </li>
+              </ul>
+        </div>
       </div>
   </div>
-  <!--/.row-->
-</div>
-<!--/.container-->
-<!--/.footer-->
 
-<div class="footer-bottom"  style="margin-top: 30px;">
+    <!--Copyright-->
+  <div class="footer-bottom"  style="margin-top: 30px;">
     <div class="container">
         <p class="pull-left"> Copyright © 2018, Lachica, Medina, Ricanor. All rights reserved.</p>
         <div class="pull-right">
@@ -87,6 +74,8 @@
             </ul>
         </div>
     </div>
-</div>
-<!--/.footer-bottom-->
+  </div>
+  <!--/.Copyright-->
+
+<!--/.Footer-->
 </footer>
