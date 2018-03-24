f06af2206c6f1c13c5231cda65f1b316a96f5975
Daniel Lachica <daniel.lachica82@gmail.com>
2018-03-24 22:37:18 +0800

    Added brand name display to view products

--- database/shoe_get.php
+++ database/shoe_get.php
@@ -21,9 +21,18 @@ $color = $row['color'];
 $size = $row['size'];
 $price = $row['price'];
 $imgpath = $row['photo_url'];
+$brandID = $row['posted_by'];
 
+mysqli_close($conn);
+
+// Get brand name
+require 'config.php';
+$sql = "SELECT brand_name FROM brands WHERE uid = $brandID";
+$result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+$row = mysqli_fetch_assoc($result);
+$postedBy = $row['brand_name'];
 	
-$shoeDetails = array($name, $description, $color, $size, $price, $imgpath);
+$shoeDetails = array($name, $description, $color, $size, $price, $imgpath, $postedBy);
 array_push($_SESSION['selected_shoe_details'], $shoeDetails);
 
 mysqli_close($conn);
