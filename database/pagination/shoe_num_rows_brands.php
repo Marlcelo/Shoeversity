39686fd40e2d7a1177705b66687022dd1c035e28
Daniel Lachica <daniel.lachica82@gmail.com>
2018-03-29 22:32:59 +0800

    addded pagination to brands products grid

--- database/pagination/shoe_num_rows_brands.php
+++ database/pagination/shoe_num_rows_brands.php
@@ -11,15 +11,17 @@ else
 	$records_per_page = 9; //default
 
 # Query for total number of records
-require 'config.php';
+require '../config.php';
 
-$sql = "SELECT * FROM shoes WHERE posted_by = " . $brandID;
+$sql = "SELECT COUNT(*) FROM shoes WHERE posted_by = " . $brandID . ";";
 
 $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+$row = mysqli_fetch_assoc($result);
 
 mysqli_close($conn);
 
-$total_records = mysqli_num_rows($result);
+// $total_records = mysqli_num_rows($result);
+$total_records = $row['COUNT(*)'];
 $num_pages = ceil($total_records/$records_per_page);
 
 echo $num_pages;
--- views/index.php
+++ views/index.php
@@ -164,7 +164,88 @@
     <!-- Include Javascript files -->
     <script src="../js/animate-products-quicklink.js"></script>
     <script src="../js/smooth-scroll.js"></script>
-    <script src="../js/ajax-paginate-grid.js"></script>
+    <!-- <script src="../js/ajax-paginate-grid.js"></script> -->
+    <script type="text/Javascript">
+        $(document).ready(function(){
+            /* Declare Global variables */
+            var current_page_id = 1;
+            var num_pages = 1;
+            var records_per_page = 9;
+
+            get_num_pages(); // update num_pages
+            load_data(current_page_id); // initialize
+
+            function load_data(page) {
+                $.ajax({
+                    url:"../database/pagination/shoe_grid_show.php",
+                    method:"POST",
+                    data:{page:page, records:records_per_page},
+                    success:function(data){
+                        $('#products-list').html(data);
+
+                        var btnPrev = document.getElementById('prev-page');
+                        var btnNext = document.getElementById('next-page');
+                        var btnCurr = document.getElementById(page);
+
+                        if(page == 1) {
+                            btnPrev.disabled = true;
+                            btnNext.disabled = false;
+                        }
+                        else if(page == num_pages) {
+                            btnPrev.disabled = false;
+                            btnNext.disabled = true;
+                        }
+
+                        btnCurr.classList.add('btn-primary');
+                    }
+                })
+            }
+
+            function get_num_pages() {
+                $.ajax({
+                    url:"../database/pagination/shoe_num_rows.php",
+                    method:"POST",
+                    data:{records:records_per_page},
+                    success:function(data){
+                        num_pages = data;
+                    }
+                })
+            }
+
+            function load_prev_page(pageID) {
+                setCurrentPageID(pageID - 1);
+                load_data(pageID - 1);
+            }
+
+            function load_next_page(pageID) {
+                setCurrentPageID(pageID + 1);
+                load_data(pageID + 1);
+            }
+
+            function setCurrentPageID(pageID) {
+                current_page_id = pageID;
+            }
+
+            /*********************************************/
+
+            // Current Page Button
+            $(document).on('click', '.pagination-link', function() {
+                var page = $(this).attr("id");
+                setCurrentPageID(parseInt(page));
+                load_data(page);
+            });
+
+            // Previous Button
+            $(document).on('click', '#prev-page', function() {
+                load_prev_page(current_page_id);
+            });
+
+            // Next Button
+            $(document).on('click', '#next-page', function() {
+                load_next_page(current_page_id);
+            });
+        });
+    </script>
     
 </body>
 </html>
