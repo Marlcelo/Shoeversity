1048876f58bccde5e21b298f98d63d1021611466
Daniel Lachica <daniel.lachica82@gmail.com>
2018-04-02 01:46:46 +0800

    added remove filter functionality

--- database/pagination/shoe_grid_show_public.php
+++ database/pagination/shoe_grid_show_public.php
@@ -33,11 +33,11 @@ mysqli_close($conn);
 # Render table with rows from query
 $colCounter = 1;
 
-if(isset($_SESSION['grid_applied_filters'])) {
+if($_SESSION['grid_sql'] != "SELECT * FROM shoes") {
 	$output .= "<div class='col-md-12' style='margin-bottom:30px; padding: 20px 15px; border-bottom: 3px solid #0097A7; background:#eee'>"; 
 	$output .= 		$_SESSION['grid_applied_filters'];
 	$output .= 		"<span class='pull-right' style='font-size: 37px; cursor: pointer; position: absolute; right: 15px; top: 1px;'>";
-	$output .= 			"<a href='../views/index.php' style='color: #999'>&times;</a>";
+	$output .= 			"<a href='../database/shoe_filter_clear.php' style='color: #999'>&times;</a>";
 	$output .= 		"</span>";
 	$output .= "</div>";
 }
