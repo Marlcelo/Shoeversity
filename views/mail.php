8143b8d322d2fbb992f4555f02f65b9a81e11189
Daniel L <daniel.lachica82@gmail.com>
2018-04-03 21:31:17 +0800

    Added library PHPMailer

---
+++ views/mail.php
@@ -0,0 +1,41 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../libs/PHPMailer/src/Exception.php';
+require '../libs/PHPMailer/src/PHPMailer.php';
+require '../libs/PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer;
+
+$mail->isSMTP();                            // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';             // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                     // Enable SMTP authentication
+$mail->Username = 'shoeversityofficial@gmail.com';          // SMTP username
+$mail->Password = 'FlashByte'; // SMTP password
+$mail->SMTPSecure = 'tls';                  // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                          // TCP port to connect to
+
+$mail->setFrom('shoeversityofficial@gmail.com', 'Shoeversity Official');
+// $mail->addReplyTo('shoeversityofficial@gmail.com', 'Shoeversity Official');
+$mail->addAddress('daniel.lachica82@gmail.com');   // Add a recipient
+// $mail->addCC('cc@example.com');
+// $mail->addBCC('bcc@example.com'); 
+
+$mail->isHTML(true);  // Set email format to HTML
+
+$bodyContent = '<h1>How to Send Email using PHP in Localhost by CodexWorld</h1>';
+$bodyContent .= '<p>This is the HTML email sent from localhost using PHP script by <b>CodexWorld</b></p>';
+
+$mail->Subject = 'Email from Localhost by CodexWorld';
+$mail->Body    = $bodyContent;
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+
+?>
