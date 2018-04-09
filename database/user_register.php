9e85e2a195f681706204a863108cb7fb20e1e5d4
Daniel L <daniel.lachica82@gmail.com>
2018-04-09 21:23:58 +0800

    Finalized user - register w/ email send

--- database/user_register.php
+++ database/user_register.php
@@ -77,7 +77,7 @@
 				}
 
 		//********************************************************
-		// STORE TO SITE_USERS TABLE
+		// SEND EMAIL TO NEW USER
 		//********************************************************
 				require '../libs/PHPMailer/PHPMailerAutoload.php';
 
@@ -90,32 +90,39 @@
 				$mail->SMTPAuth = true;                               // Enable SMTP authentication
 				$mail->Username = 'shoeversityofficial@gmail.com';    // SMTP username
 				$mail->Password = 'FlashByte';                        // SMTP password
-				$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, ssl or tls
-				$mail->Port = 587;                                    // TCP port to connect to
+				$mail->SMTPSecure = 'ssl';                            // Enable SSL encryption
+				$mail->Port = 465;                                    // TCP port to connect to for SSL
 
 				$mail->setFrom('shoeversityofficial@gmail.com', 'Shoeversity Official');
 				$mail->addAddress($_POST['email']);               
+				$mail->IsHTML(true);  
+				$mail->AddEmbeddedImage('../images/logos/shoeversity-logo.jpg', 'logo_shoeversity');
 
 				$mail->Subject = 'Welcome to Shoeversity!';
 				$message = '
+						<div style="position: fixed; top: 0px; height: auto; width: auto; padding: 7px 15px; background: #10263e; color: #fff">
+							<h2>My Account: '.$_POST['uname'].'</h2>
+						</div>
 						<div style="height:auto; width: auto; padding: 80px 110px; background: #eee; color: #121212 !important; text-align: center;">
 
-							<h1>Welcome to Shoeversity!</h1>
+							<img src="cid:logo_shoeversity" style="border-radius: 50%">
+							
+							<h1 style="margin-top: 40px">Welcome to Shoeversity!</h1>
 
-							<h3>Thank you for registering with us.</h3>
+							<h3>Hi, '.$_POST['fname'].' '.$_POST['lname'].'! Thank you for registering with us.</h3>
 
-							<p>To get started, sign in to your account <a href="localhost/Shoeversity/views/login.php">here.</a></p>
+							<p style="color: #121212 !important">To get started, sign in to your account <a href="localhost/Shoeversity/views/login.php">here</a>.</p>
 
 							<br><hr><br>
 
-							<div style="text-align: left !important; line-height: 3px">
+							<div style="text-align: left !important; line-height: 5px">
 								<p style="color:#777">Regards,</p>
-								<i style="color:#777">Shoeversity Official</i>
+								<i style="color:#777">The Shoeversity Team</i>
 							</div>
 						</div>
 					';
-				// $message = "'".$_POST['email']."'";
-				$mail->Body    = $message;
+				
+				$mail->Body = $message;
 
 				if(!$mail->send()) {
 				    echo 'Message could not be sent.';
