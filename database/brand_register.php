bf3920f8c51384a2e18bcee73941c4af87eb2e2c
Daniel Lachica <daniel.lachica82@gmail.com>
2018-04-12 00:38:03 +0800

    brand send email upon register

--- database/brand_register.php
+++ database/brand_register.php
@@ -290,57 +290,57 @@ if($row['col'] == 'TRUE'){ //Same username/email as another account
 //********************************************************
 // SEND EMAIL TO NEW BRAND
 //********************************************************
-// require '../libs/PHPMailer/PHPMailerAutoload.php';
+require '../libs/PHPMailer/PHPMailerAutoload.php';
 
-// $mail = new PHPMailer;
+$mail = new PHPMailer;
 
-// // $mail->SMTPDebug = 4;                              // Enable verbose debug output
+// $mail->SMTPDebug = 4;                              // Enable verbose debug output
 
-// $mail->isSMTP();                                      // Set mailer to use SMTP
-// $mail->Host = 'smtp.gmail.com';  					  // Specify main SMTP servers
-// $mail->SMTPAuth = true;                               // Enable SMTP authentication
-// $mail->Username = 'shoeversityofficial@gmail.com';    // SMTP username
-// $mail->Password = 'FlashByte';                        // SMTP password
-// $mail->SMTPSecure = 'ssl';                            // Enable SSL encryption
-// $mail->Port = 465;                                    // TCP port to connect to for SSL
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  					  // Specify main SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'shoeversityofficial@gmail.com';    // SMTP username
+$mail->Password = 'FlashByte';                        // SMTP password
+$mail->SMTPSecure = 'ssl';                            // Enable SSL encryption
+$mail->Port = 465;                                    // TCP port to connect to for SSL
 
-// $mail->setFrom('shoeversityofficial@gmail.com', 'Shoeversity Official');
-// $mail->addAddress($_POST['email']);               
-// $mail->IsHTML(true);  
-// $mail->AddEmbeddedImage('../images/logos/shoeversity-logo.jpg', 'logo_shoeversity');
+$mail->setFrom('shoeversityofficial@gmail.com', 'Shoeversity Official');
+$mail->addAddress($_POST['email']);               
+$mail->IsHTML(true);  
+$mail->AddEmbeddedImage('../images/logos/shoeversity-logo.jpg', 'logo_shoeversity');
 
-// $mail->Subject = 'Welcome to Shoeversity!';
-// $message = '
-// <div style="position: fixed; top: 0px; height: auto; width: auto; padding: 7px 15px; background: #10263e; color: #fff">
-// <h2>My Account: '.$_POST['uname'].'</h2>
-// </div>
-// <div style="height:auto; width: auto; padding: 80px 110px; background: #eee; color: #121212 !important; text-align: center;">
+$mail->Subject = 'Welcome to Shoeversity!';
+$message = '
+<div style="position: fixed; top: 0px; height: auto; width: auto; padding: 7px 15px; background: #10263e; color: #fff">
+<h2>My Account: '.$_POST['uname'].'</h2>
+</div>
+<div style="height:auto; width: auto; padding: 80px 110px; background: #eee; color: #121212 !important; text-align: center;">
 
-// <img src="cid:logo_shoeversity" style="border-radius: 50%">
+<img src="cid:logo_shoeversity" style="border-radius: 50%">
 
-// <h1 style="margin-top: 40px">Welcome to Shoeversity!</h1>
+<h1 style="margin-top: 40px">Welcome to Shoeversity!</h1>
 
-// <h3>Hi, '.$_POST['brandname'].'! Thank you for registering your brand with us.</h3>
+<h3>Hi, '.$_POST['brandname'].'! Thank you for registering your brand with us.</h3>
 
-// <p style="color: #121212 !important">You'."'".'re one step closer to posting your products for sale on our platform! Unfortunately, your brand registration hasn'."'".'t been verified with our <u>Site Administrators</u> yet. Don'."'".'t worry, though. We'."'".'ll send you an email once you can <a href="localhost/Shoeversity/views/login.php">sign in</a>. Cheers!</p>
+<p style="color: #121212 !important">You'."'".'re one step closer to posting your products for sale on our platform! Unfortunately, your brand registration hasn'."'".'t been verified with our <u>Site Administrators</u> yet. Don'."'".'t worry, though. We'."'".'ll send you an email once you can <a href="localhost/Shoeversity/views/login.php">sign in</a>. Cheers!</p>
 
-// <br><hr><br>
+<br><hr><br>
 
-// <div style="text-align: left !important; line-height: 5px">
-// <p style="color:#777">Regards,</p>
-// <i style="color:#777">The Shoeversity Team</i>
-// </div>
-// </div>
-// ';
+<div style="text-align: left !important; line-height: 5px">
+<p style="color:#777">Regards,</p>
+<i style="color:#777">The Shoeversity Team</i>
+</div>
+</div>
+';
 
-// $mail->Body = $message;
+$mail->Body = $message;
 
-// if(!$mail->send()) {
-// 	echo 'Message could not be sent.';
-// 	echo 'Mailer Error: ' . $mail->ErrorInfo;
-// } else {
-// 	echo 'Message has been sent';
-// }
+if(!$mail->send()) {
+	echo 'Message could not be sent.';
+	echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+	echo 'Message has been sent';
+}
 }
 
 ?>
