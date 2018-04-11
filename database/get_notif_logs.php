026b54862e87991897f037924c5776b11103971b
Daniel Lachica <daniel.lachica82@gmail.com>
2018-04-11 14:04:16 +0800

    fixed SQL query

--- database/get_notif_logs.php
+++ database/get_notif_logs.php
@@ -1,7 +1,7 @@
 <?php
 
 	require 'config.php';
-	$sql = "SELECT * FROM logs LIMIT 0,5";
+	$sql = "SELECT * FROM logs ORDER BY time_stamp DESC LIMIT 0,5";
 	$result = mysqli_query($conn, $sql);
 
 	$notifications = array();
