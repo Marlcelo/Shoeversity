4b7ce9056c27800f9661fd318baec2a18e1c4b3a
Marl Christian Ricanor <32050672+Marlcelo@users.noreply.github.com>
2018-04-12 08:28:23 +0800

    Fixed errors messages

--- views/register_brand.php
+++ views/register_brand.php
@@ -9,7 +9,7 @@
 		<div class="col-md-6">	
 			<div class="input-group">
 				<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-				<input type="text" class="form-control" name="uname" placeholder="Username" required>
+				<input type="text" class="form-control" name="uname" placeholder="Username" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter and at least 8 or more characters" required>
 			</div><br>
 		</div>
 	</div>
